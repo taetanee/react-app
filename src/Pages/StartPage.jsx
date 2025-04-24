4d47c5883dec9313f2267132d6f57ac67c7a729a
taetanee <xpxksl@gmail.com>
2025-04-25 05:47:47 +0900

    data 받아오게 수정

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -1,30 +1,40 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 
 export default function StartPage() {
-    const getData = async () => {
+    const [data, setData] = useState([]); // 데이터 저장용 상태
 
+    const getData = async () => {
         const res = await fetch(
             "https://jsonplaceholder.typicode.com/comments"
         ).then((res) => res.json());
 
-        const initData = res.slice(0, 20).map((it) => {
-            return {
-                author: it.email,
-                content: it.body,
-                emotion: Math.floor(Math.random() * 5) + 1,
-                created_date: new Date().getTime(),
-                //id: dataId.current++,
-            };
-        });
-        console.log(initData);
-    };
+        const initData = res.slice(0, 20).map((it) => ({
+            author: it.email,
+            content: it.body,
+            emotion: Math.floor(Math.random() * 5) + 1,
+            created_date: new Date().getTime(),
+        }));
 
+        setData(initData);
+    };
 
     useEffect(() => {
         getData();
     }, []);
 
-    const data = "데이터";
+    return (
+        <div>
+            <h1>Start본문</h1>
+            <h2>데이터 수 : {data.length}</h2>
 
-    return <h1>Start본문{data}</h1>;
+            {/* 데이터 리스트 보여주기 예시 */}
+            <ul>
+                {data.map((item, idx) => (
+                    <li key={idx}>
+                        <b>{item.author}</b>: {item.content}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
