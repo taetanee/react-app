bae5a2e6d64eae9f9338c709f78029e7d6c3adb6
taetanee <xpxksl@gmail.com>
2025-04-25 06:44:00 +0900

    내 로컬 데이터 가져오기

--- src/Pages/Page02.jsx
+++ src/Pages/Page02.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Page02() {
-    return <h1>데이터 가져오기2</h1>;
+    const [uuid, setUuid] = useState(""); // uuid 하나를 문자열로 저장
+
+    useEffect(() => {
+        const fetchWord = async () => {
+            try {
+                const response = await fetch("http://localhost:18080/test/getUuid");
+                const result = await response.text(); // 문자열로 응답 받는 경우
+                console.log("받은 UUID:", result);
+                setUuid(result); // 상태에 저장
+            } catch (error) {
+                console.error("에러 발생:", error);
+            }
+        };
+
+        fetchWord();
+    }, []);
+
+    return (
+        <div>
+            <h1>데이터 가져오기2</h1>
+            <h2>받은 UUID: {uuid}</h2>
+        </div>
+    );
 }
